91cdfb10699442ec866bc6b9dae96b55f5c557e6
HENG, Rattanak Vichboth <116492254+RattanakVicboth@users.noreply.github.com>
2025-06-04 23:12:32 +0700

    Update api-gateway.js

--- api-gateway.js
+++ api-gateway.js
@@ -41,7 +41,7 @@ app.use('/student',authToken, authRole('student'), (req, res) => {
 //REDIRECT TO THE TEACHER MICROSERVICE
 app.use('/teacher', authToken, authRole('teacher'),(req, res) => {
     console.log("INSIDE API GATEWAY TEACHER ROUTE")
-    proxy.web(req, res, { target: 'http://localhost:5001' });
+    proxy.web(req, res, { target: 'http://52.90.104.181:5001' });
 })
 
 //REDIRECT TO THE LOGIN(Authentication) MICROSERVICE
